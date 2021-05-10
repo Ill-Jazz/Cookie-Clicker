da0eb3cf6358a1075087134583a45f045fec6dcf
Ill-Jazz <iliasguedoura@gmail.com>
2021-05-10 20:17:14 +0200

    adding multiplier per click

--- JS/script.js
+++ JS/script.js
@@ -4,24 +4,38 @@ First: 1 cookie that provides a cookie per click
  */
 
 let count = 0;
+const score = document.querySelector("#scoreId")
 const store = document.querySelector("#storeId")
 const clickCounter = document.querySelector("#cookiesCounter");
 const button = document.querySelector("#bigCookie");
-button.addEventListener("click",counterMouse) ;
+button.addEventListener("click", counterMouse);
 
 function counterMouse() {
     count += 1;
     clickCounter.innerHTML = count + " Cookies";
+    score.innerHTML = count;
+
     if (count === 10) {
         let multiplyCookie = document.createElement("button");
-        multiplyCookie.setAttribute("id,", "multiplierId")
+        multiplyCookie.setAttribute("id", "multiplierId")
         store.append(multiplyCookie);
+
         const multiplier = document.querySelector("#multiplierId");
-        multiplier.addEventListener("click", multiply);
+        multiplier.addEventListener("click", multiply)
 
-        function multiply(){
-
+        function multiply() {
+            clickCounter.innerHTML = (count += 2) + " Cookies";
         }
 
-}
-}
+
+        /*const purchaser = document.querySelector("#multiplierId");
+        purchaser.addEventListener("click", purchase);
+
+        function purchase(){
+            let purchased = (count-=20);
+            score.innerHTML = purchased;
+
+        }
+*/
+
+    }}
